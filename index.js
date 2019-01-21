89af43fb04d860b50aece2d845b03b176fdc83cb
organized-quotation-1910 <nicholas.moore5@icloud.com>
2019-01-21 15:26:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,17 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+<<<<<<< HEAD
   if (string == "I love you, Grandma."){
     return "I love you, too."
   } else if (string !== string.toUpperCase()){
     return "I can't hear you!"
+=======
+  if (string !== string.toUpperCase()){
+    return "I can't hear you!"
+  } else if (string == "I love you, Grandma."){
+    return "I love you, too."
+>>>>>>> 869c06c970b31eebeb1d01cd27e700dc06da3de1
   } else {
     return "YES INDEED!"
   }
